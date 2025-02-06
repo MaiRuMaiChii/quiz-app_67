6bd46557b3b9bb677f68d621bfd7045f358de88a
MaiRuMaiChii <neiyneiy456@gmail.com>
2025-02-06 11:23:49 +0700

    Update quiz and score.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ export const DataContext=createContext()
 
 function App() {
   const [appState, setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
+
   return (
-    <DataContext.Provider value={{appState,setAppState}}>
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
 
    <div className="App">
     <h1>Web Development Quiz</h1>
